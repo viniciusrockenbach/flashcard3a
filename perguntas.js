e7dd12404231bf7a5a58c297ff1d08a8dfd4bf84
viniciusrockenbach <108743491+viniciusrockenbach@users.noreply.github.com>
2024-10-24 10:14:56 -0300

    uyu

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'programacao'
-    'o que é Python?'
+    'programacao',
+    'o que é Python?',
     'O Python é uma linguagm de programacao'
 )
 
 criaCartao(
-    'Geografia'
-    'Qual a capital da França?'
+    'Geografia',
+    'Qual a capital da França?',
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programacao'
-    'O que é uma funcao?'
+    'Programacao',
+    'O que é uma funçao?',
     'Uma funcao é uma tarefa é um bloco de codigo que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inhglesa'
-    'como se diz oi em ingles?'
+    'Lingua inhglesa',
+    'como se diz oi em ingles?',
     'Oi em ingles é HI (RAI)'
 )
